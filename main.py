d38b51cb9f11c2a2c8dd970c99af73f599e5df7a
0xsysr3ll <0xsysr3ll@pm.me>
2023-05-20 22:21:31 +0200

    Fixed "app not responding"

--- main.py
+++ main.py
@@ -22,10 +22,10 @@ async def daily_task():
     current_date = arrow.now()
     channel = bot.get_channel(discord_channel)
     # Check if it's Monday (0 means Monday)
-    if current_date.weekday() == 0 and current_date.hour == 8:
-        await send_week_events(channel)
-    elif current_date.hour == 8:
-        await send_today_events(channel)
+    if current_date.weekday() == 5:
+        await send_week_events(None, channel)
+    elif current_date.weekday() != 0 and current_date.hour == 8:
+        await send_today_events(None, channel)
         
 @bot.event
 async def on_ready():
@@ -40,11 +40,13 @@ async def on_ready():
     print("Done!")
 
 @bot.command(name='ping', description="Sends the bot's latency.") # this decorator makes a slash command
-async def ping_command(ctx): # a slash command will be created with the name "ping"
-    await ctx.respond(f"Pong! ({round(bot.latency * 1000)}ms)")
+async def ping_command(ctx):
+    # Perform the latency calculation
+    latency = round(bot.latency * 1000)
+    # Edit the loading message with the actual response
+    await ctx.respond(f"Pong! ({latency}ms)")
 
-
-async def send_today_events(channel):
+async def send_today_events(ctx, channel):
     today = arrow.now().format('YYYY-MM-DD')
     events = parse_calendar(today, today)
     # Get all unique words in event names
@@ -57,14 +59,19 @@ async def send_today_events(channel):
     if "PRESENTIEL" in event_name_words:
         event_mode.append("PRESENTIEL")
     if len(events) == 0:
-        await channel.send(f"Bonjour ! Il n'y a pas de cours de prévu aujourd'hui ! 📅")
+        message = "Bonjour ! Il n'y a pas de cours de prévu aujourd'hui ! 📅"
+        if ctx is not None:
+            await ctx.defer(invisible=True)
+            await ctx.respond(message)
+        else:
+            await channel.send(message)
     else:
         # Group events by day
         events_by_day = defaultdict(list)
         for event in events:
             events_by_day[event.begin.format('YYYY-MM-DD')].append(event)
         # Create the embed title
-        embed_title = "📅 Bonjour ! voici l'agenda d'aujourd'hui"
+        embed_title = "📅 Agenda d'aujourd'hui"
         if event_mode:
             embed_title += f" ({', '.join(event_mode)})"
 
@@ -82,11 +89,15 @@ async def send_today_events(channel):
                 events_str += f"\n**{event.name}**\nDe {event.begin.format('HH:mm')} à {event.end.format('HH:mm')}\n{description}{lieu}"
             embed.add_field(name=f"{arrow.get(day, 'YYYY-MM-DD').format('dddd D MMMM YYYY', locale='fr').capitalize()}", value=events_str, inline=True)
         
-        await channel.send(embed=embed)
+        if ctx is not None:
+            await ctx.defer(invisible=True)
+            await ctx.respond(embed=embed)
+        else:
+            await channel.send(embed=embed)
 
 @bot.command(name='today', description="Sends today's events.")
 async def today_command(ctx):
-    await send_today_events(ctx.channel)
+    await send_today_events(ctx, ctx.channel)
 
 @bot.command(name='tomorrow', description="Sends tomorrow's events.")
 async def tomorrow_command(ctx):
@@ -102,14 +113,15 @@ async def tomorrow_command(ctx):
     if "PRESENTIEL" in event_name_words:
         event_mode.append("PRESENTIEL")
     if len(events) == 0:
-        await ctx.respond(f"Bonjour ! Il n'y a pas de cours de prévu demain ! 📅")
+        await ctx.defer(invisible=True)
+        await ctx.respond("Bonjour ! Il n'y a pas de cours de prévu demain ! 📅")
     else:
         # Group events by day
         events_by_day = defaultdict(list)
         for event in events:
             events_by_day[event.begin.format('YYYY-MM-DD')].append(event)
         # Create the embed title
-        embed_title = "📅 Bonjour ! Voici l'agenda pour demain"
+        embed_title = "📅 Agenda pour demain"
         if event_mode:
             embed_title += f" ({', '.join(event_mode)})"
 
@@ -126,15 +138,17 @@ async def tomorrow_command(ctx):
                 lieu = f"Lieu: {event.location}\n" if event.location else ""
                 events_str += f"\n**{event.name}**\nDe {event.begin.format('HH:mm')} à {event.end.format('HH:mm')}\n{description}{lieu}"
             embed.add_field(name=f"{arrow.get(day, 'YYYY-MM-DD').format('dddd D MMMM YYYY', locale='fr').capitalize()}", value=events_str, inline=True)
+        
+        await ctx.defer(invisible=True)
         await ctx.respond(embed=embed)
 
 
 @bot.command(name='week', description="Sends this week's events.")
 async def week_command(ctx):
-    await send_week_events(ctx.channel)
+    await send_week_events(ctx, ctx.channel)
 
 # Refactor the logic into a separate function
-async def send_week_events(channel):
+async def send_week_events(ctx, channel):
     today = arrow.now().format('YYYY-MM-DD')
     week = arrow.now().shift(days=+7).format('YYYY-MM-DD')
     events = parse_calendar(today, week)
@@ -148,7 +162,12 @@ async def send_week_events(channel):
     if "PRESENTIEL" in event_name_words:
         event_mode.append("PRESENTIEL")
     if len(events) == 0:
-        await channel.send(f"Pas de cours cette semaine ! 📅")
+        message = "Bonjour ! Il n'y a pas de cours de prévu cette semaine ! 📅"
+        if ctx is not None:
+            await ctx.defer(invisible=True)
+            await ctx.respond(message)
+        else:
+            await channel.send(message)
     else:
         # Group events by day
         events_by_day = defaultdict(list)
@@ -173,7 +192,11 @@ async def send_week_events(channel):
                 events_str += f"\n**{event.name}**\nDe {event.begin.format('HH:mm')} à {event.end.format('HH:mm')}\n{description}{lieu}"
             embed.add_field(name=f"{arrow.get(day, 'YYYY-MM-DD').format('dddd D MMMM YYYY', locale='fr').capitalize()}", value=events_str, inline=True)
 
-        await channel.send(embed=embed)
+        if ctx is not None:
+            await ctx.defer(invisible=True)
+            await ctx.respond(embed=embed)
+        else:
+            await channel.send(embed=embed)
 
 
 @bot.command(name='3days', description="Sends the events for the next 3 days.")
@@ -191,6 +214,7 @@ async def threedays_command(ctx):
     if "PRESENTIEL" in event_name_words:
         event_mode.append("PRESENTIEL")
     if len(events) == 0:
+        await ctx.defer(invisible=True)
         await ctx.respond(f"Bonjour ! Il n'y a pas de cours de prévu les 3 prochains jours ! 📅")
     else:
         # Group events by day
@@ -198,7 +222,7 @@ async def threedays_command(ctx):
         for event in events:
             events_by_day[event.begin.format('YYYY-MM-DD')].append(event)
         # Create the embed title
-        embed_title = "📅 Bonjour, voici l'agenda pour les 3 prochains jours"
+        embed_title = "📅 Agenda pour les 3 prochains jours"
         if event_mode:
             embed_title += f" ({', '.join(event_mode)})"
 
@@ -216,6 +240,7 @@ async def threedays_command(ctx):
                 events_str += f"\n**{event.name}**\nDe {event.begin.format('HH:mm')} à {event.end.format('HH:mm')}\n{description}{lieu}"
             embed.add_field(name=f"{arrow.get(day, 'YYYY-MM-DD').format('dddd D MMMM YYYY', locale='fr').capitalize()}", value=events_str, inline=True)
 
+        await ctx.defer(invisible=True)
         await ctx.respond(embed=embed)
 
 bot.run(discord_token)
