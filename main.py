6621f56d9d6d81a961bc0bab0a3d8df554d2e065
0xsysr3ll <0xsysr3ll@pm.me>
2023-05-21 12:45:18 +0200

    Added some comments

--- main.py
+++ main.py
@@ -6,24 +6,26 @@ from ics import Calendar
 import arrow
 from collections import defaultdict
 
-config = Config('config/config.yml')  # Create the Config instance
-config.load()  # Load the config file
+# Load the configuration
+config = Config('config/config.yml')
+config.load()
 
+# Get discord and calendar details from the config
 discord_token = config.get_config('discord', 'token')
 discord_channel = config.get_config('discord', 'channel')
 discord_guild = config.get_config('discord', 'guild')
 calendar_url = config.get_config('calendar', 'url')
 
-# this is the only parameter that is not optional
+# Initialize the bot
 bot = discord.Bot(guild_ids=[discord_guild])
+
+# Define a daily task that sends events for the week or for today
 
 
 @tasks.loop(hours=24)
 async def daily_task():
-    # Get current date
     current_date = arrow.now()
     channel = bot.get_channel(discord_channel)
-    # Check if it's Monday (0 means Monday)
     today = arrow.now().format('YYYY-MM-DD')
     week = arrow.now().shift(days=+7).format('YYYY-MM-DD')
     if current_date.weekday() == 0 and current_date.hour == 8:
@@ -31,59 +33,61 @@ async def daily_task():
     elif current_date.weekday() != 0 and current_date.hour == 8:
         await send_events(None, channel, today, today, 'pour aujourd\'hui')
 
+# Define the bot's behavior when it's ready
+
 
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}!")
     print(f"Connected to {len(bot.guilds)} guilds!")
-    print(f"Updating calendar...")
     download_calendar(calendar_url)
-    print("Syncing commands...")
     await bot.sync_commands(force=True)
-    print("Done!")
-    print(f"Starting daily task...")
     daily_task.start()
-    print("Done!")
+
+# Define a ping command that sends the bot's latency
 
 
-# this decorator makes a slash command
 @bot.command(name='ping', description="Sends the bot's latency.")
 async def ping_command(ctx):
-    # Perform the latency calculation
     latency = round(bot.latency * 1000)
-    # Edit the loading message with the actual response
     await ctx.respond(f"Pong! ({latency}ms)")
 
 
+# This function is used to send a list of events to a Discord channel
 async def send_events(ctx, channel, start_date, end_date, time_range):
+    # Parse the calendar events between the start and end dates
     events = parse_calendar(start_date, end_date)
-    # Get all unique words in event names
+
+    # Create a set of all unique words in event names for checking presence of certain words later
     event_name_words = set(
         word for event in events for word in event.name.split())
 
-    # Check for "DISTANCIEL" and "PRESENTIEL" in event names
+    # Check for presence of "DISTANCIEL" and "PRESENTIEL" in event names and store them in event_mode
     event_mode = []
     if "DISTANCIEL" in event_name_words:
         event_mode.append("DISTANCIEL")
     if "PRESENTIEL" in event_name_words:
         event_mode.append("PRESENTIEL")
 
+    # If there are no events, create an embed message saying no courses are planned
     if len(events) == 0:
         embed_title = f"📅 Agenda {time_range}"
         embed_description = f"Bonjour ! Il n'y a pas de cours prévu {time_range} !"
         embed = discord.Embed(
             title=embed_title, description=embed_description, color=discord.Color.blue())
     else:
+        # If there are events, group them by day
         events_by_day = defaultdict(list)
         for event in events:
             events_by_day[event.begin.format('YYYY-MM-DD')].append(event)
 
+        # Create an embed message to display the events
         embed_title = f"📅 Agenda {time_range}"
         if event_mode:
             embed_title += f" ({', '.join(event_mode)})"
-
         embed = discord.Embed(title=embed_title, color=discord.Color.orange())
 
+        # For each day, add a field to the embed message with the events for that day
         for day, events in sorted(events_by_day.items(), key=lambda item: arrow.get(item[0], 'YYYY-MM-DD')):
             events_str = ""
             for event in sorted(events, key=lambda e: e.begin):
@@ -95,48 +99,94 @@ async def send_events(ctx, channel, start_date, end_date, time_range):
             embed.add_field(name=arrow.get(day, 'YYYY-MM-DD').format('dddd D MMMM YYYY',
                             locale='fr').capitalize(), value=events_str, inline=True)
 
+    # If this is a Discord interaction, respond to the interaction with the embed message
     if ctx is not None and isinstance(ctx, discord.Interaction):
-        # If this is an interaction, send the actual response
         await ctx.send(embed=embed)
     else:
+        # Otherwise, send the embed message to the provided channel
         await channel.send(embed=embed)
 
 
+# This function is a command that sends today's events to the Discord channel
 @bot.command(name='today', description="Sends today's events.")
 async def today_command(ctx):
-    await ctx.defer(ephemeral=True)  # Defer the interaction
+    # Defer the interaction to ensure the command doesn't time out
+    await ctx.defer(ephemeral=True)
+
+    # Get the current date in YYYY-MM-DD format
     today = arrow.now().format('YYYY-MM-DD')
+
+    # Set the time range string for today
     time_range = "pour aujourd'hui"
+
+    # Send a response to indicate processing is underway
     await ctx.respond('Traitement de la demande en cours... :hourglass_flowing_sand:')
+
+    # Call the send_events function to fetch and send the events for today
     await send_events(ctx, ctx.channel, today, today, time_range)
 
 
+# This function is a command that sends tomorrow's events to the Discord channel
 @bot.command(name='tomorrow', description="Sends tomorrow's events.")
 async def tomorrow_command(ctx):
-    await ctx.defer(ephemeral=True)  # Defer the interaction
+    # Defer the interaction to ensure the command doesn't time out
+    await ctx.defer(ephemeral=True)
+
+    # Get the date for tomorrow in YYYY-MM-DD format
     tomorrow = arrow.now().shift(days=+1).format('YYYY-MM-DD')
+
+    # Set the time range string for tomorrow
     time_range = "pour demain"
+
+    # Send a response to indicate processing is underway
     await ctx.respond('Traitement de la demande en cours... :hourglass_flowing_sand:')
+
+    # Call the send_events function to fetch and send the events for tomorrow
     await send_events(ctx, ctx.channel, tomorrow, tomorrow, time_range)
 
 
+# This function is a command that sends the events for the next 3 days to the Discord channel
 @bot.command(name='3days', description="Sends the events for the next 3 days.")
 async def threedays_command(ctx):
-    await ctx.defer(ephemeral=True)  # Defer the interaction
+    # Defer the interaction to ensure the command doesn't time out
+    await ctx.defer(ephemeral=True)
+
+    # Get the current date in YYYY-MM-DD format
     today = arrow.now().format('YYYY-MM-DD')
+
+    # Get the date for three days from now in YYYY-MM-DD format
     threedays = arrow.now().shift(days=+3).format('YYYY-MM-DD')
+
+    # Set the time range string for the next three days
     time_range = "pour les 3 prochains jours"
+
+    # Send a response to indicate processing is underway
     await ctx.respond('Traitement de la demande en cours... :hourglass_flowing_sand:')
+
+    # Call the send_events function to fetch and send the events for the next three days
     await send_events(ctx, ctx.channel, today, threedays, time_range)
 
 
+# This function is a command that sends the events for the current week to the Discord channel
 @bot.command(name='week', description="Sends this week's events.")
 async def week_command(ctx):
-    await ctx.defer(ephemeral=True)  # Defer the interaction
+    # Defer the interaction to ensure the command doesn't time out
+    await ctx.defer(ephemeral=True)
+
+    # Get the current date in YYYY-MM-DD format
     today = arrow.now().format('YYYY-MM-DD')
+
+    # Get the date for one week from now in YYYY-MM-DD format
     week = arrow.now().shift(days=+7).format('YYYY-MM-DD')
+
+    # Set the time range string for the current week
     time_range = "pour cette semaine"
+
+    # Send a response to indicate processing is underway
     await ctx.respond('Traitement de la demande en cours... :hourglass_flowing_sand:')
+
+    # Call the send_events function to fetch and send the events for the current week
     await send_events(ctx, ctx.channel, today, week, time_range)
+
 
 bot.run(discord_token)
--- modules/framagenda.py
+++ modules/framagenda.py
@@ -6,34 +6,36 @@ import locale  # for setting the locale
 import os
 
 locale.setlocale(locale.LC_ALL, 'fr_FR.UTF-8')  # set the locale
-
 filename = os.path.join(os.path.dirname(__file__), '../files/calendar.ics')
 
-
 def download_calendar(url):
-    # Make sure the directory exists
+    # Ensure the directory for the calendar file exists
     os.makedirs(os.path.dirname(filename), exist_ok=True)
+    
+    # Send a GET request to the calendar URL
     response = requests.get(url)
+    
+    # If the response status code is 200 (successful), write the response content to the calendar file
     if response.status_code == 200:
         with open(filename, 'wb+') as file:
             file.write(response.content)
         print(f"Calendar downloaded and saved to {filename}")
     else:
+        # If the response status code is not 200, print an error message
         print("Failed to download the calendar")
 
 
 def parse_calendar(start_date, end_date):
-    # Load the calendar file
+    # Open the calendar file and read its contents into a Calendar object
     with open(filename, 'r') as calendar_file:
         calendar = Calendar(calendar_file.read())
 
-    # Convert the input dates to arrow date objects for comparison
+    # Convert the input start and end dates (in 'YYYY-MM-DD' format) to arrow date objects
     start_date = arrow.get(start_date, 'YYYY-MM-DD').date()
     end_date = arrow.get(end_date, 'YYYY-MM-DD').date()
 
-    # Filter events based on start and end date
-    events = [event for event in calendar.events if start_date <=
-              event.begin.date() <= end_date]
+    # Create a list of events that start on or between the start and end dates
+    events = [event for event in calendar.events if start_date <= event.begin.date() <= end_date]
 
-    # Return the filtered events
+    # Return the list of filtered events
     return events
